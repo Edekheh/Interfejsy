81d3661f3f10eb364fdbb6b3bb505fb80d916bf9
Edekheh <dawidmoscicki@gmail.com>
2021-04-07 12:34:55 -0400

    initial commit

---
+++ interfejsy/scripts/test.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# Python 2/3 compatibility imports
+from __future__ import print_function
+from six.moves import input
+
+import time
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi, fabs, cos, sqrt
+
+try:
+    from math import tau
+except:  # For Python 2 compatibility
+    from math import pi
+
+    tau = 2.0 * pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+
+def dist(p, q):#missing function for carthesial path planning
+    return sqrt((p[0] - q[0]) ** 2 + (p[1] - q[1]) ** 2 + (p[2] - q[2]) ** 2)
+
+
+def all_close(goal, actual, tolerance):
+    """
+  Convenience method for testing if the values in two lists are within a tolerance of each other.
+  For Pose and PoseStamped inputs, the angle between the two quaternions is compared (the angle
+  between the identical orientations q and -q is calculated correctly).
+  @param: goal       A list of floats, a Pose or a PoseStamped
+  @param: actual     A list of floats, a Pose or a PoseStamped
+  @param: tolerance  A float
+  @returns: bool
+  """
+    if type(goal) is list:
+        for index in range(len(goal)):
+            if abs(actual[index] - goal[index]) > tolerance:
+                return False
+
+    elif type(goal) is geometry_msgs.msg.PoseStamped:
+        return all_close(goal.pose, actual.pose, tolerance)
+
+    elif type(goal) is geometry_msgs.msg.Pose:
+        x0, y0, z0, qx0, qy0, qz0, qw0 = pose_to_list(actual)
+        x1, y1, z1, qx1, qy1, qz1, qw1 = pose_to_list(goal)
+        # Euclidean distance
+        d = dist((x1, y1, z1), (x0, y0, z0))
+        # phi = angle between orientations
+        cos_phi_half = fabs(qx0 * qx1 + qy0 * qy1 + qz0 * qz1 + qw0 * qw1)
+        return d <= tolerance and cos_phi_half >= cos(tolerance / 2.0)
+
+    return True
+
+
+class MoveGroupPythonInterfaceTutorial(object):
+    def __init__(self):
+        super(MoveGroupPythonInterfaceTutorial, self).__init__()
+        ## Init node :
+        ## First initialize `moveit_commander`_ and a `rospy`_ node:
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+        ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+        ## kinematic model and the robot's current joint states
+        robot = moveit_commander.RobotCommander()
+        ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+        ## for getting, setting, and updating the robot's internal understanding of the
+        ## surrounding world:
+        scene = moveit_commander.PlanningSceneInterface()
+        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+        ## to a planning group (group of joints).  In this tutorial the group is the primary
+        ## arm joints in the Panda robot, so we set the group's name to "panda_arm".
+        ## If you are using a different robot, change this value to the name of your robot
+        ## arm planning group.
+        ## This interface can be used to plan and execute motions:
+        group_name = "panda_arm"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+        ## Getting Basic Information
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+        # We can print the name of the end-effector link for this group:
+        eef_link = move_group.get_end_effector_link()
+        #print("============ End effector link: %s" % eef_link)
+
+        # We can get a list of all the groups in the robot:
+        group_names = robot.get_group_names()
+        #print("============ Available Planning Groups:", robot.get_group_names())
+
+        # Sometimes for debugging it is useful to print the entire state of the
+        # robot:
+        #print("============ Printing robot state")
+        #print(robot.get_current_state())
+        #print("")
+
+        # Misc variables
+        self.box_name = ''
+        self.robot = robot
+        self.scene = scene
+        self.move_group = move_group
+        self.eef_link = eef_link
+        self.group_names = group_names
+
+    def go_to_joint_state(self, a , b , c , d , e , f , g ):
+        move_group = self.move_group
+        ## Planning to a Joint Goal
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^
+        ## The Panda's zero configuration is at a `singularity <https://www.quora.com/Robotics-What-is-meant-by-kinematic-singularity>`_, so the first
+        ## thing we want to do is move it to a slightly better configuration.
+        ## We use the constant `tau = 2*pi <https://en.wikipedia.org/wiki/Turn_(angle)#Tau_proposals>`_ for convenience:
+        # We get the joint values from the group and change some of the values:
+        joint_goal = move_group.get_current_joint_values()
+        joint_goal[0] = a
+        joint_goal[1] = b
+        joint_goal[2] = c
+        joint_goal[3] = d
+        joint_goal[4] = e
+        joint_goal[5] = f
+        joint_goal[6] = g
+
+        # The go command can be called with joint values, poses, or without any
+        # parameters if you have already set the pose or joint target for the group
+        move_group.go(joint_goal, wait=True)
+
+        # Calling ``stop()`` ensures that there is no residual movement
+        move_group.stop()
+        # For testing:
+        current_joints = move_group.get_current_joint_values()
+        return all_close(joint_goal, current_joints, 0.01)
+
+    def go_to_pose_goal(self, a, b, c, d):
+        move_group = self.move_group
+        ## Planning to a Pose Goal
+        ## ^^^^^^^^^^^^^^^^^^^^^^^
+        ## We can plan a motion for this group to a desired pose for the
+        ## end-effector:
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.w = d
+        pose_goal.position.x = a
+        pose_goal.position.y = b
+        pose_goal.position.z = c
+
+        move_group.set_pose_target(pose_goal)
+
+        ## Now, we call the planner to compute the plan and execute it.
+        plan = move_group.go(wait=True)
+        # Calling `stop()` ensures that there is no residual movement
+        move_group.stop()
+        # It is always good to clear your targets after planning with poses.
+        # Note: there is no equivalent function for clear_joint_value_targets()
+        move_group.clear_pose_targets()
+        current_pose = self.move_group.get_current_pose().pose
+        return all_close(pose_goal, current_pose, 0.01)
+
+    def plan_cartesian_path(self, scale=1):
+        move_group = self.move_group
+        ## Cartesian Paths
+        ## ^^^^^^^^^^^^^^^
+        ## You can plan a Cartesian path directly by specifying a list of waypoints
+        ## for the end-effector to go through. If executing  interactively in a
+        ## Python shell, set scale = 1.0.
+        ##
+        waypoints = []
+
+        wpose = move_group.get_current_pose().pose
+        wpose.position.z -= scale * 0.1  # First move up (z)
+        wpose.position.y += scale * 0.2  # and sideways (y)
+        waypoints.append(copy.deepcopy(wpose))
+
+        wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
+        waypoints.append(copy.deepcopy(wpose))
+
+        wpose.position.y -= scale * 0.1  # Third move sideways (y)
+        waypoints.append(copy.deepcopy(wpose))
+
+        # We want the Cartesian path to be interpolated at a resolution of 1 cm
+        # which is why we will specify 0.01 as the eef_step in Cartesian
+        # translation.  We will disable the jump threshold by setting it to 0.0,
+        # ignoring the check for infeasible jumps in joint space, which is sufficient
+        # for this tutorial.
+        (plan, fraction) = move_group.compute_cartesian_path(
+            waypoints,  # waypoints to follow
+            0.01,  # eef_step
+            0.0)  # jump_threshold
+
+        # Note: We are just planning, not asking move_group to actually move the robot yet:
+        return plan, fraction
+
+    def execute_plan(self, plan):
+        move_group = self.move_group
+        ## Executing a Plan
+        ## ^^^^^^^^^^^^^^^^
+        ## Use execute if you would like the robot to follow
+        ## the plan that has already been computed:
+        move_group.execute(plan, wait=True)
+
+        ## **Note:** The robot's current joint state must be within some tolerance of the
+        ## first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
+
+    def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
+        box_name = self.box_name
+        scene = self.scene
+        ## Ensuring Collision Updates Are Received
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        ## If the Python node dies before publishing a collision object update message, the message
+        ## could get lost and the box will not appear. To ensure that the updates are
+        ## made, we wait until we see the changes reflected in the
+        ## ``get_attached_objects()`` and ``get_known_object_names()`` lists.
+        ## For the purpose of this tutorial, we call this function after adding,
+        ## removing, attaching or detaching an object in the planning scene. We then wait
+        ## until the updates have been made or ``timeout`` seconds have passed
+        start = rospy.get_time()
+        seconds = rospy.get_time()
+        while (seconds - start < timeout) and not rospy.is_shutdown():
+            # Test if the box is in attached objects
+            attached_objects = scene.get_attached_objects([box_name])
+            is_attached = len(attached_objects.keys()) > 0
+
+            # Test if the box is in the scene.
+            # Note that attaching the box will remove it from known_objects
+            is_known = box_name in scene.get_known_object_names()
+
+            # Test if we are in the expected state
+            if (box_is_attached == is_attached) and (box_is_known == is_known):
+                return True
+
+            # Sleep so that we give other threads time on the processor
+            rospy.sleep(0.1)
+            seconds = rospy.get_time()
+
+        # If we exited the while loop without returning then we timed out
+        return False
+
+    def add_box(self, timeout=4):
+        box_name = self.box_name
+        scene = self.scene
+        ## Adding Objects to the Planning Scene
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        ## First, we will create a box in the planning scene between the fingers:
+        box_pose = geometry_msgs.msg.PoseStamped()
+        box_pose.header.frame_id = "panda_hand"
+        box_pose.pose.orientation.w = 1.0
+        box_pose.pose.position.z = 0.11  # above the panda_hand frame
+        box_name = "box"
+        scene.add_box(box_name, box_pose, size=(0.075, 0.075, 0.075))
+        self.box_name = box_name
+        return self.wait_for_state_update(box_is_known=True, timeout=timeout)
+
+    def attach_box(self, timeout=4):
+        box_name = self.box_name
+        robot = self.robot
+        scene = self.scene
+        eef_link = self.eef_link
+        group_names = self.group_names
+        ## Attaching Objects to the Robot
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        ## Next, we will attach the box to the Panda wrist. Manipulating objects requires the
+        ## robot be able to touch them without the planning scene reporting the contact as a
+        ## collision. By adding link names to the ``touch_links`` array, we are telling the
+        ## planning scene to ignore collisions between those links and the box. For the Panda
+        ## robot, we set ``grasping_group = 'hand'``. If you are using a different robot,
+        ## you should change this value to the name of your end effector group name.
+        grasping_group = 'hand'
+        touch_links = robot.get_link_names(group=grasping_group)
+        scene.attach_box(eef_link, box_name, touch_links=touch_links)
+        # We wait for the planning scene to update.
+        return self.wait_for_state_update(box_is_attached=True, box_is_known=False, timeout=timeout)
+
+    def detach_box(self, timeout=4):
+        #detach box from gripper function
+        box_name = self.box_name
+        scene = self.scene
+        eef_link = self.eef_link
+        scene.remove_attached_object(eef_link, name=box_name)
+        return self.wait_for_state_update(box_is_known=True, box_is_attached=False, timeout=timeout)
+
+    def remove_box(self, timeout=4):
+        #remove box from scene function
+        box_name = self.box_name
+        scene = self.scene
+        scene.remove_world_object(box_name)
+        return self.wait_for_state_update(box_is_attached=False, box_is_known=False, timeout=timeout)
+
+
+
+
+def main():
+    print("")
+    print("----------------------------------------------------------")
+    print("Welcome to Moveit presentation !")
+    print("----------------------------------------------------------")
+    input("============ Press `Enter` to begin animation ...")
+    tutorial = MoveGroupPythonInterfaceTutorial()#starting node
+    print("Robot will perform few joint interpolation moves")
+    print("Basing robot, then script will add and attach box to the gripper !")
+    time.sleep(2)
+    tutorial.go_to_joint_state(0,-pi/4,0,-pi/2,0,pi/3,0)#base pos
+    tutorial.add_box()
+    tutorial.attach_box()
+    print("Planning cartesial path . After plan is done, script will move the arm and detach box")
+    cartesian_plan, fraction = tutorial.plan_cartesian_path(scale=3)
+    tutorial.execute_plan(cartesian_plan)
+    tutorial.detach_box()
+    print("Rebasing robot, then removing box from animation")
+    tutorial.go_to_joint_state(0,-pi/4,0,-pi/2,0,pi/3,0)
+    tutorial.remove_box()
+    print("Robot now will perform inverse kinematic movement")
+    tutorial.go_to_pose_goal(0.4,0.1,0.4,1.0)
+    print("Basing robot for next tasks ...")
+    tutorial.go_to_joint_state(0,-pi/4,0,-pi/2,0,pi/3,0)#base pos
+    print("Thank You for watching presentation !")
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
